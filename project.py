36f01b620e5ded208fabf7b6c5a04876cbc43592
inessarana <inessarana@gmail.com>
2021-10-12 10:55:39 +0100

    Update project.py

--- project.py
+++ project.py
@@ -1,29 +1,18 @@
 #%% Import Libraries
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.preprocessing import linear_model
+import os
+from sklearn import linear_model
 import os
 
 #%% Load testing and training data
-cd = "/Users/ana/Documents/Ana/universidade/5 ano 1 semestre/Aprendizagem Automática/MachineLearningProject" #working directory
-x_test_1=np.load(cd+"/Xtest_Regression_Part1.npy")
-x_train_1=np.load(cd+"/Xtrain_Regression_Part1.npy")
-y_train_1=np.load(cd+"/Ytrain_Regression_Part1.npy")
-
-x_test_2=np.load(cd+"/Xtest_Regression_Part2.npy")
-x_train_2=np.load(cd+"/Xtrain_Regression_Part2.npy")
-y_train_2=np.load(cd+"/Ytrain_Regression_Part2.npy")
-
-del cd
-
-#%% Load variables for Inês until I can figure this out
 cd = os.getcwd()
-x_train1 = np.load(cd+'/Data/Xtrain_Regression_Part1.npy')
-x_train2 = np.load(cd+'/Data/Xtrain_Regression_Part2.npy')
-y_train1 = np.load(cd+'/Data/Ytrain_Regression_Part1.npy')
-y_train2 = np.load(cd+'/Data/Ytrain_Regression_Part2.npy')
-x_test1 = np.load(cd+'/Data/Xtest_Regression_Part1.npy')
-x_test2 = np.load(cd+'/Data/Xtest_Regression_Part2.npy')
+x_train_1 = np.load(cd+'/Data/Xtrain_Regression_Part1.npy')
+x_train_2 = np.load(cd+'/Data/Xtrain_Regression_Part2.npy')
+y_train_1 = np.load(cd+'/Data/Ytrain_Regression_Part1.npy')
+y_train_2 = np.load(cd+'/Data/Ytrain_Regression_Part2.npy')
+x_test_1 = np.load(cd+'/Data/Xtest_Regression_Part1.npy')
+x_test_2 = np.load(cd+'/Data/Xtest_Regression_Part2.npy')
 
 del cd
 
@@ -46,86 +35,9 @@ del cd
 #             plt.title("feature " +str(i)+" vs feature " +str(j))
             
 # The features seem independent
+         
 
-#%% Linear regression 
-
-def lr_par(xtrain,ytrain):# Calculates the beta parameters of linear regression
-    c=len(xtrain) #number of observations
-    #f=len(xt[0])
-    X=np.append(np.ones((c,1)),xtrain,axis=1)
-    Xtrans=np.transpose(X)
-    
-    beta=np.matmul(np.matmul(np.linalg.inv(np.matmul(Xtrans, X)),Xtrans),ytrain)
-    return beta
-
-
-def lr(xtest,beta): #performs linear regression to xtest with the beta parameters given by the above function
-    c=len(xtest)
-    X=np.append(np.ones((c,1)),xtest,axis=1)
-    ytest=np.matmul(X,beta)
-    return ytest
-
-def lrpredictor(x_train,y_train,x_test): # predicts y based on training with x_train and y_train (its the result of the merging of the last two functions)
-    ytest=lr(x_test,lr_par(x_train,y_train))
-    return ytest
-
-def calcsse(ytest_pred,ytest_true): # Calculates sum of squared errors
-    sse=0
-    for i in range(len(ytest_pred)):
-        sse=sse+float((ytest_pred[i]-ytest_true[i])**2)
-    return sse
-
-#%% k Fold classification linear regression
-
-def crossvalidation(k,x_train,y_train):
-    c=len(x_train)
-     
-    if (c%k)!=0:
-        print("Cannot compute. Choose a divider of "+str(c))
-        return
-    elif k==1:
-        print("Cannot perform 1-fold classification since there is no test set.")
-        return
-    
-    else:
-        sse_vector=[] # vector where the sse of each fold will be stored
-        f=len(x_train[0]) #number of features
-        fold=int(c/k) #size of each fold
-        
-        Xpart=np.split(x_train,k) #list of k partitions of x_train
-        Ypart=np.split(y_train,k)
-        
-        for i in range(len(Xpart)):
-            
-            #build matrices xtrain and ytrain
-            xtrain=np.empty((fold,f))
-            ytrain=np.empty((fold,1))
-            for j in range(len(Xpart)):
-                if j==i:
-                    xtest=Xpart[j]
-                    ytest_true=Ypart[j]
-                    
-                else:
-                    xtrain=np.vstack((xtrain, Xpart[j]))
-                    ytrain=np.vstack((ytrain, Ypart[j]))
-            
-            xtrain=xtrain[fold:,:]
-            ytrain=ytrain[fold:,:]
-
-            #test
-            ytest_pred= lrpredictor(xtrain,ytrain,xtest)
-                    
-            # calculate sum of squared errors
-            sse=calcsse(ytest_pred,ytest_true)/fold # normalized by the size of each fold
-            sse_vector=sse_vector+[sse]
-            
-    print("The mean SSE for "+str(k)+" folds is "+str(np.mean(sse_vector)))
-    return np.mean(sse_vector)                                                
-
-a=crossvalidation(5,x_train_1,y_train_1)               
-
-#%% ANA VÊ ESTA SECÇÃO
-    # continuei o que estava a fazer a pouco e vou por aqui as funções que sairam de lá para compararmos e escolhermos uma            
+#%% PREDICTORS EVALUATION
 
 # PREDICTOR 1: LINEAR REGRESSION
 
@@ -227,14 +139,10 @@ def cross_val(xt,yt,k,func,l):
     for i in range(k):
         errors[i] = sse(y_test[i,:],y_pred[i,:])/fold
     
-    print("The mean SSE for "+str(k)+"-folds is "+str(np.mean(errors)))
+    print('The mean SSE for '+str(k)+'-folds using predictor '+func+' is '+str(np.mean(errors)))
     return np.mean(errors)       
     
-<<<<<<< HEAD
-cross_val(x_train1,y_train1,5,'lr',1)   
-=======
-cross_val(x_train_1,y_train_1,5,'lr',1)   
->>>>>>> 5123fc3391cb52353124f216183da9babdb2ef41
+cross_val(x_train_1,y_train_1,10,'lasso',0.01)   
 
 #%% Test function
 crossvalidation(1,x_train_1,y_train_1)               
