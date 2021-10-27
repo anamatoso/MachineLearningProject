3336fbfd8f1861e0a0a0b7aa87bb75b3df8e4e8b
Ana Matoso <anamatoso92@gmail.com>
2021-10-27 16:20:35 +0100

    Update project2.py

--- project2.py
+++ project2.py
@@ -7,7 +7,9 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import tensorflow as tf
 from tensorflow import keras
-
+from tensorflow.keras import layers
+from tensorflow.keras.layers import CenterCrop
+from tensorflow.keras.layers import Rescaling
 
 #%% LOAD TEST AND TRAINING DATA
 cd = os.getcwd()
@@ -16,6 +18,11 @@ cd = os.getcwd()
 x_train_1 = np.load(cd+'/Data/Xtrain_Classification_Part1.npy')
 y_train_1 = np.load(cd+'/Data/Ytrain_Classification_Part1.npy')
 x_test_1 = np.load(cd+'/Data/Xtest_Classification_Part1.npy')
+
+xtrainreshape = np.reshape(x_train_1,(6513,50,50))
+xtestreshape = np.reshape(x_test_1,(1134,50,50))
+
+
 
 # Part 2
 # x_train_2 = np.load(cd+'/Data/Xtrain_Classification_Part2.npy')
@@ -29,3 +36,51 @@ del cd
 data = x_train_1[1030]
 data_forimage = np.reshape(data,(50,50))
 plt.imshow(data_forimage,cmap='gray', vmin=0, vmax=255)
+
+
+
+#%% Create Layers
+
+inputs = keras.Input(shape=(50, 50, 1))
+# Center-crop images to 150x150
+x = CenterCrop(height=50, width=50)(inputs)
+# Rescale images to [0, 1]
+x = Rescaling(scale=1.0 / 255)(x)
+
+# Apply some convolution and pooling layers
+x = layers.Conv2D(filters=32, kernel_size=(2, 2), activation="relu")(x)
+x = layers.MaxPooling2D(pool_size=(2, 2))(x)
+x = layers.Conv2D(filters=32, kernel_size=(2, 2), activation="relu")(x)
+x = layers.MaxPooling2D(pool_size=(2, 2))(x)
+x = layers.Conv2D(filters=32, kernel_size=(2, 2), activation="relu")(x)
+
+# Apply global average pooling to get flat feature vectors
+x = layers.GlobalAveragePooling2D()(x)
+
+# Add a dense classifier on top
+num_classes = 2
+outputs = layers.Dense(num_classes, activation="softmax")(x)
+
+model = keras.Model(inputs=inputs, outputs=outputs)
+model.summary()
+
+processed_data = model(xtrainreshape)
+print(processed_data.shape)
+
+model.compile(optimizer=keras.optimizers.RMSprop(learning_rate=1e-3),loss=keras.losses.CategoricalCrossentropy())
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+
+batch_size=32
+
+val_dataset = tf.data.Dataset.from_tensor_slices((xtrainreshape[5210:], y_train_1[5210:])).batch(batch_size)
+dataset = tf.data.Dataset.from_tensor_slices((xtrainreshape[0:5210], y_train_1[0:5210])).batch(batch_size)
+
+# history=model.fit(dataset, epochs=1, validation_data=val_dataset)
+history=model.fit(xtrainreshape[0:5210], y_train_1[0:5210],batch_size=batch_size, epochs=10)
+print(history)
+
+predictions = model.predict(xtrainreshape[5210:])
+print(predictions)
+
+
+
